47bd794105ef77a0e13cd5b3c0390328b47d2a1a
fred9702 <fred9702@gmail.com>
2020-04-26 20:43:13 +0100

    AuthContext

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import SignUp from './SignUp';
+import { AuthProvider } from './Auth';
 
 function App() {
   return (
-   <h1>Coronavirus Count Application</h1> 
+    <AuthProvider>
+      <Router>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/signup" component={SignUp}/>
+      </Router>
+    </AuthProvider>
   );
 }
 
